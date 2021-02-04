3525d93fbe22aca16b235390464edd6d7ecf1d56
weidj <wei7wei@126.com>
2021-02-04 17:43:04 +0800

    Thymeleaf

---
+++ SpringBootThymeleafTest/src/main/java/com/dj/launch/controller/SpringTest.java
@@ -0,0 +1,13 @@
+package com.dj.launch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpringTest {
+    @RequestMapping("test")
+    public  String freemarkerTest(){
+        System.out.println("ceshi");
+        return "thymeleaftemp";
+    }
+}

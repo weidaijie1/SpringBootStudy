81f1cb0d0a3aa8f0a91900defb3123639ab95d92
weidj <wei7wei@126.com>
2021-02-20 14:16:11 +0800

    发送html邮件

--- SpringBootEmailSend/src/main/java/com/dj/launch/MailService.java
+++ SpringBootEmailSend/src/main/java/com/dj/launch/MailService.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class MailService {
@@ -25,6 +28,22 @@ public class MailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(content);
+        mailSender.send(message);
+    }
+
+    /**
+     * 发送html邮件
+     */
+    public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+        //注意这里使用的是MimeMessage
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        //第二个参数是否是html，true表示发送的邮件正文是html文本
+        helper.setText(content, true);
+
         mailSender.send(message);
     }
 
--- SpringBootEmailSend/src/test/java/com/dj/launch/MailServiceTest.java
+++ SpringBootEmailSend/src/test/java/com/dj/launch/MailServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
 
 //用配置文件的指定端口DEFINED_PORT作为启动端口运行测试用例
 
@@ -26,5 +27,16 @@ public class MailServiceTest {
         mailService.sendSimpleMail("951687336@qq.com",
                 "普通文本邮件",
                 "普通文本邮件内容测试");
+    }
+
+    @Test
+    public void sendHtmlMail() throws MessagingException {
+        mailService.sendHtmlMail("951687336@qq.com","一封html测试邮件","<body style=\"text-align: center;margin-left: auto;margin-right: auto;\">\n"
+                + " <div id=\"welcome\" style=\"text-align: center;position: absolute;\" >\n"
+                +"      <h3>\"一封html测试邮件\"</h3>\n"
+                +"      <span>http://www.zimug.com</span>"
+                + "     <div style=\"text-align: center; padding: 10px\"><a style=\"text-decoration: none;\" href=\"https://zimug.com\" target=\"_bank\" >"
+                + "           <strong>我很用心，希望你有所收获</strong></a></div>\n"
+                + " </div>\n" + "</body>");
     }
 }
